bdcbfb74b56e07a44de6ac1e95adcdc31fd9bfa1
Satyabrat Sarangi <satyabratsarangi1211@gmail.com>
2026-07-10 02:20:53 +0530

    Add /api to the end of apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://crmbackend-production-3409.up.railway.app'
+  apiUrl: 'https://crmbackend-production-3409.up.railway.app/api'
 };
